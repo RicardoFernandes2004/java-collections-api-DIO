238edd43e19872883b5d4263e510f86f9847ea57
RicardoFernandes2004 <ricardo.fernandes02082004@gmail.com>
2025-05-03 18:32:29 -0300

    blablablableblebleblublulbu

--- src/br/com/dio/collections/main/Main.java
+++ src/br/com/dio/collections/main/Main.java
@@ -1,7 +1,16 @@
 package br.com.dio.collections.main;
 
+import br.com.dio.collections.bean.ListaTarefas;
+
 public class Main {
     public static void main(String[] args){
+        ListaTarefas listaTarefas = new ListaTarefas();
+        System.out.println("num list de tasks" + listaTarefas.obterNumeroTotalDeTarefas());
+
+        listaTarefas.adicionarTarefa("pinto");
+        listaTarefas.adicionarTarefa("bolas");
+        System.out.println("num list de tasks: " + listaTarefas.obterNumeroTotalDeTarefas());
+        System.out.println(listaTarefas.obterDescricoesDeTarefas());
 
     }
 }
